ba6c41ee8e3e0dcb49e394432e36cf831976bf70
Roman Polyakov <roman_polyakov@senla.eu>
2023-05-05 17:03:59 +0300

    feat: setting the env vars up

--- src/components/Counter/utils.ts
+++ src/components/Counter/utils.ts
@@ -1,15 +1,15 @@
 export const calcAvatarUrl = (name: string) => {
   switch (name) {
     case 'Roman':
-      return 'https://firebasestorage.googleapis.com/v0/b/svintus3-f2840.appspot.com/o/roma.jpg?alt=media&token=8e60ce62-9b8b-4975-9c4e-34c3d2bb7481';
+      return import.meta.env.VITE_PIC_ROMA_URL;
     case 'Elizabeth':
-      return 'https://firebasestorage.googleapis.com/v0/b/svintus3-f2840.appspot.com/o/liza.jpg?alt=media&token=8c00ba19-5a2e-4de4-83ba-4606a75f911a';
+      return import.meta.env.VITE_PIC_LIZA_URL;
     case 'Arseniy':
-      return 'https://firebasestorage.googleapis.com/v0/b/svintus3-f2840.appspot.com/o/ars.jpg?alt=media&token=1baabe9b-929b-485a-a0af-3150809a756d';
+      return import.meta.env.VITE_PIC_ARS_URL;
     case 'Dmitriy':
-      return 'https://firebasestorage.googleapis.com/v0/b/svintus3-f2840.appspot.com/o/dima.jpg?alt=media&token=57b1df27-6b50-412d-a868-a169c0bb3305';
+      return import.meta.env.VITE_PIC_DIMA_URL;
     case 'Oksana':
-      return 'https://firebasestorage.googleapis.com/v0/b/svintus3-f2840.appspot.com/o/oks.jpg?alt=media&token=ed7dc9bc-23f8-4281-b7a6-a9deae4c3d36';
+      return import.meta.env.VITE_PIC_OKS_URL;
     default:
       return '';
   }
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { getFirestore } from '@firebase/firestore';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBL1kPFlXWdoniW7ucaQrC5z4knEJSBITw',
-  authDomain: 'svintus3-f2840.firebaseapp.com',
-  projectId: 'svintus3-f2840',
-  storageBucket: 'svintus3-f2840.appspot.com',
-  messagingSenderId: '546736648325',
-  appId: '1:546736648325:web:8078aca4edf124be4439ec',
-  measurementId: 'G-D9S8RGT65H',
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+  measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
